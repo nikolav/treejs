9f24c0ceb5bad03b8cd690bad28d4ebca2cd90fa
nikolav <admin@nikolav.rs>
2022-01-31 19:56:51 +0000

    makes index module export

--- test_.js
+++ test_.js
@@ -1,19 +1,14 @@
-const tree = require("./src/tree");
+const tree = require("./index");
 
 const t = new tree("@1");
 
-t.append(t.node("n1@1"));
-t.append(t.node("n2@1"));
-t.append(t.node("n3@1"));
+t.append(t.node({ value: "n1@1" }));
+t.append(t.node({ value: "n2@1" }));
+t.append(t.node({ value: "n3@1" }));
 
-t.eq(1).append(t.node("n1@n2@1"));
-t.eq(1).append(t.node("n2@n2@1"));
+t.eq(1).append(t.node({ value: "n1@n2@1" }));
+t.eq(1).append(t.node({ value: "n2@n2@1" }));
 
-console.log(
-  t
-    .lsa()
-    .map((n) => "" + n)
-    .join(", ")
-);
+console.log(t.lsa().join(", "));
 
 //eof
